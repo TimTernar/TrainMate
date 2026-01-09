6a265c689617206af5b16d232b97b501142efc3e
TimTernar <somthing900@gmail.com>
2026-01-10 00:17:34 +0100

    Dodal workout class

---
+++ TrainMate/TrainMate/Workout.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainMate
+{
+    class Workout
+    {
+        public int Id { get; set; }
+
+        public DateTime date { get; set; }
+
+        public Dictionary<string, Exercise> exercises { get; set; }
+
+        public Workout() { }
+
+        public Workout(int id, DateTime date, Dictionary<string, Exercise> exercises)
+        {
+            Id = id;
+            this.date = date;
+            this.exercises = exercises;
+        }
+    }
+}

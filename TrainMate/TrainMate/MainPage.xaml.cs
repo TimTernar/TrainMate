a3cde34ff5f6b28fb8f1bd72d76175b3ea00744a
TimTernar <somthing900@gmail.com>
2026-01-10 14:38:49 +0100

    Naredil delujoči vpis, povezano na firebase

--- TrainMate/TrainMate/MainPage.xaml.cs
+++ TrainMate/TrainMate/MainPage.xaml.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.Maui.ApplicationModel.Communication;
 using Microsoft.Maui.Controls;
+using Firebase.Auth;
+using Firebase.Auth.Providers;
 
 
 namespace TrainMate
 {
     public partial class MainPage : ContentPage
     {
+
+        private string webApiKey = "AIzaSyDS2qmCMAA-qgB25cB_Ry7yRd7uurLKfyc";
+
+        public string? email { get; set; }
+
+        public string? password { get; set; }
+
+
         public MainPage()
         {
             InitializeComponent();
@@ -13,7 +23,36 @@ namespace TrainMate
 
         public async void OnLoginClicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new HomePage());
+            this.email = emailEntry.Text;
+            this.password = passwordEntry.Text;
+            try
+            {
+                var config = new Firebase.Auth.FirebaseAuthConfig
+                {
+                    ApiKey = webApiKey,
+                    AuthDomain = "mobilne-45354.firebaseapp.com",
+                    Providers = new FirebaseAuthProvider[]
+                    {
+                            new EmailProvider()
+                    }
+                };
+
+                var client = new FirebaseAuthClient(config);
+                var userCredential = await client.SignInWithEmailAndPasswordAsync(email, password);
+                var serializiranaVsebina = System.Text.Json.JsonSerializer.Serialize(userCredential.AuthCredential);
+
+                if (userCredential != null)
+                {
+                    Preferences.Set("SvezToken", serializiranaVsebina);
+                    await Application.Current.MainPage.DisplayAlert("Uspeh", "Prijava uspešna!", "V redu");
+                    await Navigation.PushAsync(new HomePage());
+                }
+
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Napaka", ex.Message, "V redu");
+            }
         }
 
         public async void OnRegisterClicked(object sender, EventArgs e)

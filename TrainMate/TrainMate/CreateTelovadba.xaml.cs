d690b1f3cb02974f4b37be031e217512b8c3852f
TimTernar <somthing900@gmail.com>
2026-01-14 20:24:03 +0100

    dodal stran za ustvarjanje poljubne telovadbe dodal navigacijo do nove strani

---
+++ TrainMate/TrainMate/CreateTelovadba.xaml.cs
@@ -0,0 +1,9 @@
+namespace TrainMate;
+
+public partial class CreateTelovadba : ContentPage
+{
+	public CreateTelovadba()
+	{
+		InitializeComponent();
+	}
+}
--- TrainMate/TrainMate/TelovadbePage.xaml.cs
+++ TrainMate/TrainMate/TelovadbePage.xaml.cs
@@ -9,18 +9,19 @@ public partial class TelovadbePage : ContentPage
 
     private int _counter = 2;
 
-	private void AddWorkout(object sender, EventArgs e)
+	private async void AddWorkout(object sender, EventArgs e)
 	{
 		WorkoutSelection.Add(new TextCell
 		{
+			
 			Text = $"{_counter}. New workout",
              Detail = "Dynamically added row"
+			 
 
         });
 
         _counter++;
-
-
+        await Navigation.PushAsync(new CreateTelovadba());
     }
 
 }

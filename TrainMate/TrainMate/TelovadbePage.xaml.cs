4083583d60c935781b8b207d70d5c22973f0e060
TimTernar <somthing900@gmail.com>
2026-01-15 02:41:23 +0100

    naredil delujoči prikaz imen telovadb

--- TrainMate/TrainMate/TelovadbePage.xaml.cs
+++ TrainMate/TrainMate/TelovadbePage.xaml.cs
@@ -1,16 +1,10 @@
-using Newtonsoft.Json;
-using System.Collections.ObjectModel;
-
 namespace TrainMate;
 
 public partial class TelovadbePage : ContentPage
 {
-    public ObservableCollection<Workout> Workouts { get; set; } = new();
-
     public TelovadbePage()
     {
         InitializeComponent();
-        BindingContext = this;
     }
 
     protected override async void OnAppearing()
@@ -19,49 +13,34 @@ public partial class TelovadbePage : ContentPage
         await LoadWorkoutsAsync();
     }
 
-    //tole te povede na stran CreateTelovadba ali "Create Workout"
+    private async Task LoadWorkoutsAsync()
+    {
+        using HttpClient httpClient = new();
+        string url =
+            "https://mobilne-45354-default-rtdb.europe-west1.firebasedatabase.app/Workouts.json";
+
+        var response = await httpClient.GetStringAsync(url);
+
+        var names = new List<string>();
+
+        if (response.TrimStart().StartsWith("["))
+        {
+            var workouts = Newtonsoft.Json.JsonConvert.DeserializeObject<List<dynamic>>(response);
+            foreach (var w in workouts)
+                names.Add((string)w.Name);
+        }
+        else
+        {
+            var workouts = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(response);
+            foreach (var w in workouts.Values)
+                names.Add((string)w.Name);
+        }
+
+        WorkoutList.ItemsSource = names;
+    }
+
     private async void AddWorkout(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new CreateTelovadba());
-    }
-
-    //baloži podatke o obstojeèih treningih iz firebase database-a
-    private async Task LoadWorkoutsAsync()
-    {
-        try
-        {
-            using HttpClient httpClient = new();
-            string url =
-                "https://mobilne-45354-default-rtdb.europe-west1.firebasedatabase.app/Workouts.json";
-            var response = await httpClient.GetStringAsync(url);
-
-            var data = JsonConvert.DeserializeObject<List<Workout>>(response);
-
-            if (data != null && data.Count > 0)
-            {
-                Workouts.Clear();
-                foreach (var workout in data)
-                {
-                    if (workout != null)
-                    {
-                        if (workout.Exercises != null)
-                        {
-                            foreach (var exercise in workout.Exercises.Values)
-                            {
-                                if (exercise.Sets != null)
-                                {
-                                    exercise.Sets = exercise.Sets.Where(s => s != null).ToList();
-                                }
-                            }
-                        }
-                        Workouts.Add(workout);
-                    }
-                }
-            }
-        }
-        catch (Exception ex)
-        {
-            await DisplayAlert("Napaka", $"{ex.Message}", "V redu");
-        }
     }
 }
